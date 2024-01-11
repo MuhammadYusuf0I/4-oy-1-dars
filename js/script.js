aeeabb7e85f5a6b7da248053b76be1b841f14e21
MuhammadYusuf0I <abduvasiyevmuhammadyusuf28@gmail.com>
2024-01-11 22:09:54 +0500

    issues

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// 1-misol
+
+let cub = +prompt ('Kubning tominini kiriting ?');
+const volume = cub**3;
+console.log(volume);
+
+// 2-misol
+
+let number = +prompt('Ixtiyori son kiriting');
+const cub1 = number**3;
+console.log(cub1);
+
+// 3-misol
+
+let arbitraryNumber1 = +prompt('Ixtiyori son kiriting');
+let arbitraryNumber2 = +prompt('Ixtiyori son kiriting');
+const total = arbitraryNumber1+arbitraryNumber2;
+const sumSquared = total**2;
+console.log(sumSquared);
+
+// 4-misol
+
+let arbitraryNumber = +prompt('Ixtiyori son kiriting');
+let total1 = arbitraryNumber%100;
+let sum = total1%10;
+let total2 =total1-sum;
+let sum1 = total2/10
+console.log(sum1)
+
+// 5-misol
+
+let number1 = +prompt('Ixtiyori son kiriting');
+let Sum = number1%10;
+let Sum1 = number1-Sum;
+let Sum2 = Sum1/10
+const Total = Sum+Sum2;
+console.log(Total);
+
+// 6-misol 
+
+let Number1 = +prompt('Ixtiyori son kiriting');
+let suM = Number1%100;
+let suM1 = Number1-suM;
+const suM2 = suM1/100;
+console.log(suM2);
+
+// 8-misol 
+
+let numBer = +prompt('Ixtiyori son kiriting');
+const sUm = (numBer-10)**2;
+console.log(sUm);
+
